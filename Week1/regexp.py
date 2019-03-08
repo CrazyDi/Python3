af1e688028fb71f6b9b3b367fd598ee31a81997e
Katuhis <katuhis@yandex.ru>
2019-03-08 11:26:24 +0300

    regular

---
+++ Week1/regexp.py
@@ -0,0 +1,11 @@
+def calculate(data, findall):
+    matches = findall(r"([abc])([+-]{0,1})=([abc]{0,1})([+|-]{0,1}[0-9]{0,1})")  # Если придумать хорошую регулярку, будет просто
+    for v1, s, v2, n in matches:  # Если кортеж такой структуры: var1, [sign]=, [var2], [[+-]number]
+        if s == '+':
+            data[v1] = data[v1] + data.get(v2, 0) + int(n or 0)
+        if s == '-':
+            data[v1] = data[v1] - (data.get(v2, 0) + int(n or 0))
+        else:
+            data[v1] = data.get(v2, 0) + int(n or 0)
+
+    return data

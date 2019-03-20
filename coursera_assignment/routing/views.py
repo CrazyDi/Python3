76185092879819317533ad8ada8a17db915457ec
Katuhis <katuhis@yandex.ru>
2019-03-20 17:22:45 +0300

    regular

--- coursera_assignment/routing/views.py
+++ coursera_assignment/routing/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
 import re
 
@@ -8,7 +8,7 @@ def simple_route(request):
     if request.method == 'GET':
         return HttpResponse(status=200)
     else:
-        return HttpResponse(status=405)
+        return HttpResponseNotAllowed('GET')
 
 
 def slug_route(request, slug):
@@ -19,22 +19,23 @@ def sum_route(request, a, b):
     return HttpResponse(int(a) + int(b))
 
 
-@csrf_exempt
 def sum_get_method(request):
     if request.method == 'GET':
-        if 'a' in request.GET and 'b' in request.GET:
-            if re.match('\d', request.GET['a']) and re.match('\d', request.GET['b']):
-                return HttpResponse(int(request.GET['a']) + int(request.GET['b']))
+        a = request.GET.get('a', '')
+        b = request.GET.get('b', '')
+        try:
+            return HttpResponse(int(a) + int(b))
+        except ValueError:
+            return HttpResponse(status=400)
+    return HttpResponseNotAllowed('GET')
 
-    return HttpResponse(status=400)
 
-
-@csrf_exempt
 def sum_post_method(request):
     if request.method == 'POST':
-        if 'a' in request.POST and 'b' in request.POST:
-            if re.match('\d', request.POST['a']) and re.match('\d', request.POST['b']):
-                return HttpResponse(int(request.POST['a']) + int(request.POST['b']))
-
-    return HttpResponse(status=400)
-
+        a = request.POST.get('a', '')
+        b = request.POST.get('b', '')
+        try:
+            return HttpResponse(int(a) + int(b))
+        except ValueError:
+            return HttpResponse(status=400)
+    return HttpResponseNotAllowed('POST')

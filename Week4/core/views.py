90fb80b8d12c72e093ca9c387fd610364f6702f9
Katuhis <katuhis@yandex.ru>
2019-03-26 12:38:34 +0300

    regular

---
+++ Week4/core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'core/index.html')
+
+
+def topic_details(request, pk):
+    return render(request, 'core/topic_details.html')

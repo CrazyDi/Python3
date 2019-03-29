d206b249d1359b2db26cc0fbd4119c6f60902e78
Katuhis <katuhis@yandex.ru>
2019-03-29 13:00:02 +0300

    regular

--- Week5/coursera_forms/formdummy/views.py
+++ Week5/coursera_forms/formdummy/views.py
@@ -1,9 +1,68 @@
+import json
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+from django.http import JsonResponse
 from django.shortcuts import render
-
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from marshmallow import ValidationError as MarshmallowError
+from .forms import DummyForm, ProductForm
+from .schemas import REVIEW_SCHEMA, ReviewSchema
 
 
 class FormDummyView(View):
 
     def get(self, request):
-        return render(request, 'form.html', {})
+        form = DummyForm()
+        return render(request, 'form.html', {'form': form})
+
+    def post(self, request):
+        form = DummyForm(request.POST)
+        if form.is_valid():
+            context = form.cleaned_data
+            return render(request, 'form.html', context)
+        else:
+            return render(request, 'error.html', {'error': form.errors})
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class FormProductView(View):
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            context = form.cleaned_data
+            return render(context, status=201)
+        else:
+            return render(request, 'error.html', {'error': form.errors})
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class SchemaView(View):
+
+    def post(self, request):
+        try:
+            document = json.loads(request.body)
+            validate(document, REVIEW_SCHEMA)
+            return JsonResponse(document, status=201)
+        except json.JSONDecodeError:
+            return JsonResponse({'errors': 'Invalid JSON'}, status=400)
+        except ValidationError as exc:
+            return JsonResponse({'errors': exc.message}, status=400)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class MarshView(View):
+
+    def post(self, request):
+        try:
+            document = json.loads(request.body)
+            schema = ReviewSchema(strict=True)
+            data = schema.load(document)
+            return JsonResponse(data.data, status=201)
+        except json.JSONDecodeError:
+            return JsonResponse({'errors': 'Invalid JSON'}, status=400)
+        except MarshmallowError as exc:
+            return JsonResponse({'errors': exc.messages}, status=400)
+
